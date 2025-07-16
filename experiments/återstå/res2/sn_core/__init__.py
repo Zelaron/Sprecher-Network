8349a267b2297ac15ac4e1acd2fc124579539b21
Christian Hägg <zelaron@gmail.com>
2025-07-17 00:54:00 +0200

    Add files via upload

---
+++ experiments/återstå/res2/sn_core/__init__.py
@@ -0,0 +1,38 @@
+"""Sprecher Network (SN) core package."""
+
+from .model import (
+    TheoreticalRange,
+    PhiCodomainParams,
+    SimpleSpline,
+    SprecherLayerBlock,
+    SprecherMultiLayerNetwork,
+    NodeCentricProjection
+)
+from .train import train_network, PlateauAwareCosineAnnealingLR, recalculate_bn_stats, has_batchnorm
+from .data import get_dataset, DATASETS
+from .plotting import plot_results, plot_loss_curve
+from .config import CONFIG, MNIST_CONFIG, Q_VALUES_FACTOR, PARAM_CATEGORIES
+from .export import export_parameters, parse_param_types
+
+__all__ = [
+    'TheoreticalRange',
+    'PhiCodomainParams',
+    'SimpleSpline',
+    'SprecherLayerBlock',
+    'SprecherMultiLayerNetwork',
+    'NodeCentricProjection',
+    'train_network',
+    'PlateauAwareCosineAnnealingLR',
+    'recalculate_bn_stats',
+    'has_batchnorm',
+    'get_dataset',
+    'DATASETS',
+    'plot_results',
+    'plot_loss_curve',
+    'CONFIG',
+    'MNIST_CONFIG',
+    'Q_VALUES_FACTOR',
+    'PARAM_CATEGORIES',
+    'export_parameters',
+    'parse_param_types'
+]
